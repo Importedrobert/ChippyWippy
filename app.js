eadcb7806497650c420bbb1a8daa1ac0a69be343
Importedrobert <isaac1goof@gmail.com>
2020-07-31 00:34:20 -0500

    added changetxt

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // SELECTORS
 
+const calculateButtonSelector = document.querySelector(".calculate-button");
 
 // EVENT LISTENERS
 
+calculateButtonSelector.addEventListener("click", changetxt)
 
 // VARIABLES
 
 
 // FUNCTIONS
+
+function changetxt() {
+
+    document.getElementById('replace').innerHTML = "did it work?"
+
+}
