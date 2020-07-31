b957dd13d35181de00cc572e18134281b5da3558
Importedrobert <isaac1goof@gmail.com>
2020-07-30 23:15:42 -0500

    seperated the alert

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const calculateButtonSelector = document.querySelector(".calculate-button");
 const whiteInputSelector = document.querySelector(".white-input");
 const redInputSelector = document.querySelector(".red-input");
-const greenInputSelector = document.querySelector(".green-input")
-const blueInputSelector = document.querySelector(".blue-input")
-const blackInputSelector = document.querySelector(".black-input")
+const greenInputSelector = document.querySelector(".green-input");
+const blueInputSelector = document.querySelector(".blue-input");
+const blackInputSelector = document.querySelector(".black-input");
 
 // EVENT LISTENERS
 calculateButtonSelector.addEventListener("click", calculate);
@@ -19,21 +19,29 @@ const blackChipValue = .50;
 // FUNCTIONS
 function calculate() {
     var chipValueTotal = 0;
+    var whiteChipValueT = whiteInputSelector.value;
+    var redChipValueT = redInputSelector.value;
+    var greenChipValueT = greenInputSelector.value;
+    var blueChipValueT = blueInputSelector.value;
+    var blackChipValueT = blackInputSelector.value;
+
     if (whiteInputSelector.value >=0) {
-        chipValueTotal = Number(whiteInputSelector.value) * whiteChipValue + chipValueTotal;
+        chipValueTotal = Number(whiteInputSelector.value) * whiteChipValue + whiteChipValueT;
     }
     if (redInputSelector.value >=0) {
-        chipValueTotal = Number(redInputSelector.value) * redChipValue + chipValueTotal;
+        chipValueTotal = Number(redInputSelector.value) * redChipValue + redChipValueT;
     }
     if (greenInputSelector.value >=0) {
-        chipValueTotal = Number(greenInputSelector.value) * greenChipValue + chipValueTotal;
+        chipValueTotal = Number(greenInputSelector.value) * greenChipValue + greenChipValueT;
     }
     if (blueInputSelector.value >=0) {
-        chipValueTotal = Number(blueInputSelector.value) * blueChipValue + chipValueTotal;
+        chipValueTotal = Number(blueInputSelector.value) * blueChipValue + blueChipValueT;
     }
     if (blackInputSelector.value >=0) {
-        chipValueTotal = Number(blackInputSelector.value) * blackChipValue + chipValueTotal;
+        chipValueTotal = Number(blackInputSelector.value) * blackChipValue + blackChipValueT;
     }
-    alert(chipValueTotal);
+    alert(whiteChipValueT+" White " + redChipValueT+" red " + greenChipValueT+" green " + blueChipValueT+" blue " + blackChipValueT+" black ");
 
 }
+
+
